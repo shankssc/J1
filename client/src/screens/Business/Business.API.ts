8dcf80cd3ab9d43894bcd67e60fa18ea6eb510aa
shankssc <suyashhbk@rediffmail.com>
2024-06-05 18:01:23 -0400

    feat(business): Added the Business instance updation API

--- client/src/screens/Business/Business.API.ts
+++ client/src/screens/Business/Business.API.ts
@@ -4,7 +4,9 @@ import {
         deleteBusiness,
          } from "../../graphql/mutations";
 import { listBusinesses } from "../../graphql/queries";
-import { CreateBusinessInput } from "../../API";
+import { CreateBusinessInput,
+         UpdateBusinessInput,
+         UpdateBusinessMutation } from "../../API";
 import { generateClient } from 'aws-amplify/api';
 
 const client = generateClient();
@@ -35,3 +37,44 @@ export const createNewBusiness = async (businessDetails: CreateBusinessInput) =>
     }
 }
 
+export const UpdateBusinessDetails = async (businessId: string, businessDetails: UpdateBusinessInput) => {
+    try {
+        const updatedBusiness = await client.graphql({
+            query: updateBusiness,
+            variables: {
+                input: businessDetails
+                }
+            });
+
+        return updatedBusiness;
+    } catch (error:any) {
+        console.error('Error updating business:', error);
+        throw error;
+    }
+}
+
+/*
+// Code to use if the id from updateBusinessInput is not the same as the id of the instance
+// that we are trying to update
+
+export const updateBusinessDetails = async (businessId: string, businessDetails: UpdateBusinessInput) => {
+    try {
+        const { id, ...rest } = businessDetails; // Destructure to exclude 'id' from 'businessDetails'
+        
+        const updatedBusiness = await API.graphql({
+            query: updateBusiness,
+            variables: {
+                input: {
+                    id: businessId,
+                    ...rest
+                }
+            }
+        });
+
+        return updatedBusiness; // Should be 'updatedBusiness' not 'updateBusiness'
+    } catch (error:any) {
+        console.error('Error updating business:', error);
+        throw error;
+    }
+}
+*/
